9a23e102cade57e00920105b6735eb40a1ab77cb
Shiamak Das <shiamakdas@outlook.com>
2025-12-14 16:19:26 -0500

    Fix: syntax error

--- globals.py
+++ globals.py
@@ -2,5 +2,5 @@ import pygame as pg
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 960, 540
 
-BLACK = pg.color(0, 0, 0)
-WHITE = pg.color(255, 255, 255)
+BLACK = pg.Color(0, 0, 0)
+WHITE = pg.Color(255, 255, 255)
