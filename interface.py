1fbda22d13a1eede43435cb034dcca65ba1af0d0
Shiamak Das <shiamakdas@outlook.com>
2025-12-17 20:53:01 -0500

    Create Container class and drawing method

--- interface.py
+++ interface.py
@@ -22,7 +22,31 @@ class Section:
 
 
 class Container:
-    def __init__(self, width: float, height: float, sections: list):
+    def __init__(
+        self,
+        width: float,
+        height: float,
+        sections: list[Section],
+        rows: int,
+        columns: int,
+    ):
         self.width: float = width
         self.height: float = height
-        self.sections: list = sections
+        self.sections: list[Section] = sections
+        self.rows: int = rows
+        self.columns: int = columns
+        self.surface = pg.Surface((self.width, self.height))
+
+    def draw_sections(self):
+        sections: list[Section] = self.sections
+        last_section: Section = None
+        current_x = 0
+        current_y = 0
+
+        for section in sections:
+            if last_section:
+                current_x += last_section.width
+            if current_x >= self.width:
+                current_x = 0
+                current_y += last_section.height
+            section.surface.blit(self.surface, (current_x, current_y))
